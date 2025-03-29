83abb9901db78a467619e1928460d98ef039a2cd
272853 <163885786+272853@users.noreply.github.com>
2025-03-29 18:08:54 +0100

    starvation issue

--- main.cpp
+++ main.cpp
@@ -7,18 +7,80 @@
 
 using namespace std;
 
+
+// Philosopher class
 class Philosopher {
 public:
     int id;
     bool leftFork;
     bool rightFork;
+    int lastEat;
 
-
+    bool canEat()
+        {
+            if(leftFork==true && rightFork==true){
+                return true;
+             } 
+             else{
+                return false; 
+             }   
+        }
     
 
 };
 
+int nextPhilosopher(int philosopherId, int philosopherCount);
+
+int beforePhilosopher(int philosopherId, int philosopherCount);
+
 // Quit program enytime
+void mainThreadFunction();
+
+int main(int argc, char* argv[]) 
+{
+    thread mainThread(mainThreadFunction);
+    // { 
+
+        int philosopherCount = stoi(argv[1]);
+
+        if(philosopherCount>8){
+            cout<<"Argument is too big, must be less or equal 8"<<endl;
+            cout<<"--------------------------------------------"<<endl;
+            cout<<"Press q to quit"<<endl;
+        }
+        else{
+            // Create Philosophers 
+            vector<Philosopher> philosophers(philosopherCount);
+            for (int i = 0; i < philosopherCount; i++) {
+                philosophers[i].id = i + 1;
+                philosophers[i].leftFork = false;
+                philosophers[i].rightFork = false;
+                philosophers[i].lastEat=1;
+            }
+            
+            int hungriestPhilosopherId = philosophers[0].id;
+            int maxLastEat = philosophers[0].lastEat;
+
+            while(true){
+
+                for (int i = 1; i < philosopherCount; i++) {
+                    if (philosophers[i].lastEat > maxLastEat) {
+                        maxLastEat = philosophers[i].lastEat;
+                        hungriestPhilosopherId = philosophers[i].id;
+                    }
+                }
+
+
+            }
+
+        }
+
+    // }
+    mainThread.join();
+    return 0;
+}
+
+
 void mainThreadFunction()
 {
     string quit;
@@ -30,18 +92,22 @@ void mainThreadFunction()
     }
 }
 
-int main(int argc, char* argv[]) {
-    thread mainThread(mainThreadFunction);
-    // { 
-        
-        while(true)
-        {
-            cout<<"Do..."<<endl;
-            _sleep(1000);
-        }
+int nextPhilosopher(int philosopherId, int philosopherCount)
+{
+    if(philosopherId==philosopherCount){
+        return 1;
+    }
+    else{
+        return philosopherId+1;
+    }
+}
 
-
-    // }
-    mainThread.join();
-    return 0;
+int beforePhilosopher(int philosopherId, int philosopherCount)
+{
+    if(philosopherId=1){
+        return philosopherCount;
+    }
+    else{
+        return philosopherId-1;
+    }
 }
