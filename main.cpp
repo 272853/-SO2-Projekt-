a6fe462837f9542d4d6b902c64fe0634c221b835
272853 <163885786+272853@users.noreply.github.com>
2025-03-29 21:45:02 +0100

    race condition work deadlock not working

--- main.cpp
+++ main.cpp
@@ -3,80 +3,63 @@
 #include <mutex>
 #include <vector>
 #include <chrono>
-#include <cstdlib>
 
 using namespace std;
 
-
-// Philosopher class
 class Philosopher {
 public:
     int id;
-    bool leftFork;
-    bool rightFork;
-    int lastEat;
+    mutex* leftFork;
+    mutex* rightFork;
 
-    bool canEat()
-        {
-            if(leftFork==true && rightFork==true){
-                return true;
-             } 
-             else{
-                return false; 
-             }   
-        }
-    
+    Philosopher(int i, mutex* left, mutex* right) 
+        : id(i), leftFork(left), rightFork(right) {}
 
+    void eat() {
+        // Release and block acces
+        lock_guard<mutex> leftLock(*leftFork);
+        lock_guard<mutex> rightLock(*rightFork);
+
+        cout << "Philosopher " << id << " start : " << endl;
+        this_thread::sleep_for(chrono::seconds(1)); 
+        cout << "Philosopher  " << id << " end" << endl;
+    }
 };
-
-int nextPhilosopher(int philosopherId, int philosopherCount);
-
-int beforePhilosopher(int philosopherId, int philosopherCount);
 
 // Quit program enytime
 void mainThreadFunction();
 
-int main(int argc, char* argv[]) 
-{
+int main(int argc, char* argv[]) {
+    
     thread mainThread(mainThreadFunction);
-    // { 
 
+    while(true)
+    {
         int philosopherCount = stoi(argv[1]);
-
-        if(philosopherCount>8){
-            cout<<"Argument is too big, must be less or equal 8"<<endl;
-            cout<<"--------------------------------------------"<<endl;
-            cout<<"Press q to quit"<<endl;
-        }
-        else{
-            // Create Philosophers 
-            vector<Philosopher> philosophers(philosopherCount);
-            for (int i = 0; i < philosopherCount; i++) {
-                philosophers[i].id = i + 1;
-                philosophers[i].leftFork = false;
-                philosophers[i].rightFork = false;
-                philosophers[i].lastEat=1;
-            }
-            
-            int hungriestPhilosopherId = philosophers[0].id;
-            int maxLastEat = philosophers[0].lastEat;
-
-            while(true){
-
-                for (int i = 1; i < philosopherCount; i++) {
-                    if (philosophers[i].lastEat > maxLastEat) {
-                        maxLastEat = philosophers[i].lastEat;
-                        hungriestPhilosopherId = philosophers[i].id;
-                    }
-                }
-
-
-            }
-
+        if (philosopherCount > 8) {
+            cout << "Argumet must less than 9" << endl;
+            return 1;
         }
 
-    // }
-    mainThread.join();
+        vector<mutex> forks(philosopherCount); 
+        vector<Philosopher> philosophers;
+        vector<thread> threads;
+
+        // Create Philosophers
+        for (int i = 0; i < philosopherCount; i++) {
+            philosophers.emplace_back(i, &forks[i], &forks[(i + 1) % philosopherCount]);
+        }
+
+        // Start eating
+        for (int i = 0; i < philosopherCount; i++) {
+            threads.emplace_back(&Philosopher::eat, &philosophers[i]);
+        }
+
+        for (auto& t : threads) {
+            t.join();
+        }
+    }
+
     return 0;
 }
 
@@ -89,25 +72,5 @@ void mainThreadFunction()
         if(quit=="q"||quit=="Q") {
             exit(0); 
         }
-    }
-}
-
-int nextPhilosopher(int philosopherId, int philosopherCount)
-{
-    if(philosopherId==philosopherCount){
-        return 1;
-    }
-    else{
-        return philosopherId+1;
-    }
-}
-
-int beforePhilosopher(int philosopherId, int philosopherCount)
-{
-    if(philosopherId=1){
-        return philosopherCount;
-    }
-    else{
-        return philosopherId-1;
     }
 }
